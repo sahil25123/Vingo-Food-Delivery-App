0123f1a5503886effca1144a511bfc5ecbdd0e06
Akshatsrivastava26 <akshat.srivastava1564@gmail.com>
2026-03-07 15:56:19 +0530

    feat: enhance CheckOut page with address input handling and geolocation features; integrate address fetching from Geoapify API

--- frontend/src/pages/CheckOut.jsx
+++ frontend/src/pages/CheckOut.jsx
@@ -9,8 +9,13 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import "leaflet/dist/leaflet.css";
+import axios from "axios";
+import { setAddress, setLocation } from "../redux/mapSlice";
+import { useState } from "react";
+import { useEffect } from "react";
+import { use } from "react";
 
-import { setLocation} from "../redux/mapSlice";
+
 
 function RecenterMap({location}){
   const map=useMap()
@@ -22,13 +27,47 @@ function RecenterMap({location}){
 
 function CheckOut() {
   const {location, address} = useSelector((state) => state.map);
+  const [addressInput, setAddressInput] = useState("");
   const dispatch = useDispatch();
+  const apiKey=import.meta.env.VITE_GEO_API_KEY;
   const onDragEnd = (e) => {
     const {lat, lng} = e.target._latlng;
     dispatch(setLocation({lat, lon: lng}))
+    getAddressByLatLng(lat, lng);
   }
 
-  
+  const getAddressByLatLng = async (lat, lon) => {
+    try {
+      const result = await axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&format=json&apiKey=${apiKey}`);
+      dispatch(setAddress(result?.data?.results[0].formatted || result?.data?.results[0].address_line1 || result?.data?.results[0].address_line2));
+      
+    } catch (error) {
+      console.error("Error fetching address:", error);
+      
+    }
+  }
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            dispatch(setLocation({ lat: latitude, lon: longitude }));
+            getAddressByLatLng(latitude, longitude);
+    });
+  }
+  const getLatLngByAddress = async () => {
+    try {
+      const result = await axios.get(`https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(addressInput)}&apiKey=${apiKey}`);
+      const {lat, lon} = result.data.features[0].properties;
+      dispatch(setLocation({lat, lon}));
+    }
+    catch (error) {
+      console.error("Error fetching lat/lng:", error);
+    }
+  }
+
+  useEffect(() => {
+    setAddressInput(address);
+  }, [address])
 
   const navigate = useNavigate();
   return (
@@ -38,13 +77,13 @@ function CheckOut() {
       </div>
       <div className="w-full max-w-[900px] bg-white rounded-2xl shadow-xl p-6 space-y-6">
         <h1 className="text-2xl font-bold">Checkout</h1>
-        {/* Location Section */}
+        {/* Map Location Section */}
         <section>
           <h2 className="text-lg font-semibold mb-2 flex items-center gap-2 text-gray-800"><IoLocationSharp className="text-[#ff4d2d]" />Delivery Location</h2>
           <div className="flex gap-2 mb-3">
-            <input type="text" className="flex-1 border border-gray-300 rounded-lg p-2 text-sm focus:outline-none focus:ring-2 focus:ring-[#ff4d2d]" placeholder="Enter Your Delivery Address..." value={address}/>
-            <button className="bg-[#ff4d2d] hover:bg-[#e64526] text-white px-3 py-2 rounded-lg flex items-center justify-center"><IoSearchOutline size={17} /></button>
-            <button className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-2 rounded-lg flex items-center justify-center"><TbCurrentLocation size={17} /></button>
+            <input type="text" className="flex-1 border border-gray-300 rounded-lg p-2 text-sm focus:outline-none focus:ring-2 focus:ring-[#ff4d2d]" placeholder="Enter Your Delivery Address..." value={addressInput} onChange={(e) => setAddressInput(e.target.value)}/>
+            <button className="bg-[#ff4d2d] hover:bg-[#e64526] text-white px-3 py-2 rounded-lg flex items-center justify-center cursor-pointer" onClick={getLatLngByAddress}><IoSearchOutline size={17} /></button>
+            <button className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-2 rounded-lg flex items-center justify-center cursor-pointer" onClick={getCurrentLocation}><TbCurrentLocation size={17} /></button>
           </div>
           {/* Map showing current location */}
           <div className="rounded-xl border overflow-hidden ">
@@ -52,12 +91,16 @@ function CheckOut() {
               <MapContainer className={"w-full h-full"} center={[location?.lat, location?.lon]} zoom={17} scrollWheelZoom={false}>
                 <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                 <RecenterMap location={location}/>
-                <Marker position={[location?.lat, location?.lon]} draggable={true} eventHandlers={{dragend:onDragEnd}}/>
+                <Marker position={[location?.lat, location?.lon]} draggable eventHandlers={{dragend:onDragEnd}}/>
               </MapContainer>
 
             </div>
           </div>
 
+        </section>
+        {/* Payment Section */}
+        <section>
+          <h2 className="">Payment Method</h2>
         </section>
 
       </div>
